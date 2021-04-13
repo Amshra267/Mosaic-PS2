e7c2011710c9f73f0d6b396f0da241f7b3cbb071
Mainak Samanta <mainak.samanta.ece19@itbhu.ac.in>
2021-04-13 18:21:04 +0530

    file edited

--- char_extract.py
+++ char_extract.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import imutils
+import os
 from imutils.perspective import four_point_transform
 
 # Give this function an image and direction, it will check in that direction and if encounters a white pixel it will make all other remaining pixels in that direction white.
@@ -51,6 +52,11 @@ def find_cnta(thresh_img):
 def extract_plate(gray):
 
     ret,binary = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY)
+    binary = check_h_border(binary)
+    binary = check_v_border(binary)
+    cv2.imshow("BINARY", binary)
+    cv2.waitKey(0)
+
 
     binary = cv2.bitwise_not(binary)
 
@@ -90,18 +96,23 @@ def extract_plate(gray):
     thresh_ht = fill_dirn(H, "ht")
     thresh_hb = fill_dirn(H, "hb")
     thresh_v = cv2.bitwise_and(thresh_vl, thresh_vr)
-    #thresh_h = cv2.bitwise_and(thresh_ht, thresh_hb)
+    thresh_h = cv2.bitwise_and(thresh_ht, thresh_hb)
     thresh_xx = cv2.bitwise_and(thresh_h, thresh_v)
-    cv2.imshow("THRESH_V", thresh_v)
+    #cv2.imshow("THRESH_V", thresh_v)
     nnx = np.zeros(thresh_xx.shape, np.uint8)
     cnts = cv2.findContours(thresh_v, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-    c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
+    c = sorted(cnts, key=cv2.contourArea, reverse=True)
+    if len(c)==0:
+        approx_f = np.array([[[0, 0]], [[320, 0]], [[320, 240]], [[0, 240]]])
+        return approx_f
+    else:
+        c = c[0]
     hull = cv2.convexHull(c, False)
     cv2.drawContours(nnx, [hull], 0, 255, -1, 8)
     cv2.drawContours(nnx, [hull], 0, 255, 5, 8)# after getting the mask extending its boundary to get a bigger area
     cv2.imshow("NNX", nnx)
-    #nnx_dst = nnx.copy()
+    nnx_dst = nnx.copy()
     print("HULL LENGTH: ", len(hull))
     # Tried cornerHarris but its not working properly
     # dst = cv2.cornerHarris(nnx, 5, 5, 0.1)
@@ -111,7 +122,12 @@ def extract_plate(gray):
     # print("CORNER HARRIS: ", len(dst))
     # In the final mask of the plate calculating the approx, minAreaRect will not be much suitable
     cnts, _ = cv2.findContours(nnx, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
+    c = sorted(cnts, key=cv2.contourArea, reverse=True)
+    if len(c)==0:
+        approx_f = np.array([[[0, 0]], [[320, 0]], [[320, 240]], [[0, 240]]])
+        return approx_f
+    else:
+        c = c[0]
     approx_f = cv2.approxPolyDP(c, 0.06*cv2.arcLength(c, True), True)# Tested on different images 0.6 is suitable for most of them
     
     if len(approx_f)==4:
@@ -128,6 +144,8 @@ def extract_plate(gray):
         # If mask not detected returns approx of whole image
         approx_f = np.array([[[0, 0]], [[320, 0]], [[320, 240]], [[0, 240]]])
     return approx_f
+
+
 
 
 def approx_update(aprx):
@@ -184,75 +202,118 @@ def adjust_gamma(image, gamma=1.0):
 	# apply gamma correction using the lookup table
 	return cv2.LUT(image, table)
 
+def check_h_border(thresh_img):
+    n_row, n_column = thresh_img.shape
+    thresh_img = cv2.bitwise_not(thresh_img)
+    prs = 0
+    i = 0
+    while(i<(n_row//5)):
+        row = thresh_img[i]
+        row = row/255
+        rs = np.sum(row)
+        if rs<(prs//2):
+            thresh_img[:i] = 0
+            
+        else:
+            if rs>=prs:
+                prs = rs
+        i += 5
+
+    thresh_img = cv2.flip(thresh_img, 0)
+    prs = 0
+    i = 0
+    while(i<(n_row//5)):
+        row = thresh_img[i]
+        row = row/255
+        rs = np.sum(row)
+        if rs<(prs//2):
+            thresh_img[:i] = 0
+            
+        else:
+            if rs>=prs:
+                prs = rs
+        i += 5
+    thresh_img = cv2.flip(thresh_img, 0)
+    thresh_img = cv2.bitwise_not(thresh_img)
+
+    return thresh_img
+
+def check_v_border(thresh_img):
+    n_row, n_column = thresh_img.shape
+    thresh_img = cv2.bitwise_not(thresh_img)
+    pcs = 0
+    i = 0
+    while(i<(n_column//15)):
+        column = thresh_img[:, i]
+        column = column/255
+        cs = np.sum(column)
+        if cs<(pcs//2):
+            thresh_img[:, :i] = 0
+            break
+        else:
+            if cs>=pcs:
+                pcs = cs
+            i += 5
+
+    thresh_img = cv2.flip(thresh_img, 1)
+    pcs = 0
+    i = 0
+    while(i<(n_column//15)):
+        column = thresh_img[:, i]
+        column = column/255
+        cs = np.sum(column)
+        if cs<(pcs//2):
+            thresh_img[:, :i] = 0
+            break
+        else:
+            if cs>=pcs:
+                pcs = cs
+            i += 5
+
+    thresh_img = cv2.flip(thresh_img, 1)
+    thresh_img = cv2.bitwise_not(thresh_img)
+
+    return thresh_img
+
 if __name__ == '__main__':
-    image = cv2.imread('/home/mainak/Documents/Robotics/Mosiac/Mosiac ps2/MosaicPS2/MosaicPS2/indian_plates/Mahindra-Scorpio-525108c.jpg_0353_0270_0352_0168_0060.png',cv2.IMREAD_UNCHANGED)
-    #image = cv2.imread('/home/mainak/Documents/Robotics/Mosiac/Mosiac ps2/MosaicPS2/MosaicPS2/indian_plates/Volkswagen-Vento-514293c.png',cv2.IMREAD_UNCHANGED)
+    path = input("Path: \n")
+    image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+    
 
-    ## Plate detection by Haar Cascade model tried but not giving good results
-
-    # plate_detector = cv2.CascadeClassifier('/home/mainak/Documents/Robotics/Mosiac/Mosiac ps2/haarcascade_russian_plate_number.xml')
-    # image = cv2.resize(image, (320, 240))/home/mainak/Documents/Robotics/Mosiac/Mosiac ps2/MosaicPS2/MosaicPS2/indian_plates/Volvo-XC90-524182b.png
-    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # if gray is not None:
-    #     plate = plate_detector.detectMultiScale(gray, scaleFactor=1.01, minNeighbors=6)
-    #     print(plate)
-    #     image = image[plate[1]:(plate[1]+plate[3]), plate[0]:(plate[0]+plate[2])]
-    #     print("PLATE DETECTED")
-
-    # In case of a rear view of whole car take roi of the plate portion, in case of only plate take roi of whole image
+    # in case of a rear view of whole car take roi of the plate portion, in case of only plate take roi of whole image
     image = cv2.resize(image, (320, 240))
+    image = cv2.normalize(image, image, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
     r = cv2.selectROI("ROI", image, showCrosshair=False, fromCenter=False)
     image = image[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
     image = cv2.resize(image, (320, 240))
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
+    cv2.imshow("GRAY", gray)
     # Plate Exatraction
-    approx_f = extract_plate(gray)
+    aprx_f = extract_plate(gray.copy())
     #approx_f = approx_update(approx_f)
     nnx_f = np.zeros(gray.shape, np.uint8)
-    nnx_f = cv2.drawContours(nnx_f, [approx_f], 0, 255, -1)
+    nnx_f = cv2.drawContours(nnx_f, [aprx_f], 0, 255, -1)
     cv2.imshow("NNX_F", nnx_f)
     wraped = cv2.bitwise_and(gray, gray, mask = nnx_f)
+    
     # Four point Transform (approx_f should be reshaped in size (4, 2))
-    wraped = four_point_transform(wraped, approx_f.reshape(4, 2))
+    wraped = four_point_transform(wraped, aprx_f.reshape(4, 2))
+    cv2.imshow("Wraped", wraped)
+    #wraped = cv2.resize(wraped, (320, 240))
     # Normalize image
-    norm = cv2.normalize(wraped, wraped, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
-    cv2.imshow("NORM", norm)
+    #norm = cv2.normalize(wraped, wraped, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    #cv2.imshow("NORM", norm)
 
     # Gamma adjustment and Thresholding ((80, 100) is better range)
-    wraped = adjust_gamma(wraped, gamma=1.5)
+    #wraped = adjust_gamma(wraped, gamma=1.5)
     blurred = cv2.GaussianBlur(wraped, (5, 5), 0)
     ret, thresh = cv2.threshold(wraped, 80, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-    # Edge detection tried but not performing well, welcome to modify
-
-    #thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
-    # edged = cv2.Canny(thresh, 30, 200)
-    # contours, _ = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    # screenCnt = None
-    # for cnt in contours:
-    #     a = cv2.contourArea(cnt)
-    #     if a>(0.5*edged.shape[0]*edged.shape[1]):
-    #         screenCnt = cnt
-    #     else:
-    #         break
-    # print(screenCnt)
-    # if screenCnt is not None:
-    #     x_, y_, w_, h_ = cv2.boundingRect(screenCnt)
-    #     rect = wraped[y_:(y_+h_), x_:(x_+w_)]
-    #     rect = cv2.copyMakeBorder(rect, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=255)
-        
-    #     rect = cv2.resize(rect, (480, 360))
-
-    #     rect = cv2.bilateralFilter(rect, 13, 15, 15)
-    #     #rect = gamma_correction(rect)
-    #     ret, thresh_n = cv2.threshold(rect, 90, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    # else:
+    
 
     # Detecting contours of characters and filling their inside spaces and drawing contours in another black region which filters out noise
     thresh_n = thresh.copy()
-    thresh_n = cv2.erode(thresh_n, np.zeros((3, 3), np.uint8), iterations=1)
+    #thresh_n = cv2.erode(thresh_n, np.zeros((3, 3), np.uint8), iterations=1)
     thresh_n = cv2.copyMakeBorder(thresh_n, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=255)
     cnts, _ = cv2.findContours(thresh_n, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
@@ -261,12 +322,12 @@ if __name__ == '__main__':
     for i, c in enumerate(cnts):
         rect_t = cv2.boundingRect(c)
         if (rect_t[2]*rect_t[3])<(0.1*480*360) and (rect_t[2]*rect_t[3])>500:
-            thresh_nn = cv2.drawContours(thresh_nn, [c], 0, 255, -1, 8)
+            thresh_nn = cv2.drawContours(thresh_nn, [c], 0, 255, -1)
             print(rect_t[2]*rect_t[3])
-
+    
     # From the previous black region detecting bonding rects and cropping that portion from the previous threshold image (thresh_n)
     # This time tere will be only one contour for a character at max because its inside region is filled
-    thresh_nn = cv2.erode(thresh_nn, np.ones((5, 5), np.uint8), iterations=1)
+    #thresh_nn = cv2.erode(thresh_nn, np.ones((5, 5), np.uint8), iterations=1)
     cnts, _ = cv2.findContours(thresh_nn, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
     rects = []
@@ -282,14 +343,13 @@ if __name__ == '__main__':
         # Draw a straight rectangle with the points
         s = thresh_n[y:(y+h), x:(x+w)]
         cv2.imshow(str(ind), s)
-        
 
 
-    
+    #cv2.imshow("EDGED", edged)
     cv2.imshow("THRESH", thresh)
     cv2.imshow("THRESH_N", thresh_n)
     cv2.imshow("THRESH_NN", thresh_nn)
-    cv2.imshow("Wraped", wraped)
+    #cv2.imshow("Wraped", wraped)
     cv2.imshow("BLURRED", blurred)
     cv2.imshow("original", image)
     cv2.waitKey(0)
